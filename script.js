2cf51f7fd1de52359a4f086da7ca8e5309d57343
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-28 19:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,64 +1,19 @@
-// Get the required elements
-const video = document.querySelector(".flex");
-const progressBar = document.querySelector(".progress__filled");
-const playButton = document.querySelector(".toggle");
-const volumeSlider = document.querySelector('input[name="volume"]');
-const speedSlider = document.querySelector('input[name="playbackRate"]');
-const skipButtons = document.querySelectorAll("[data-skip]");
-const speedBar = document.querySelector(".speed-bar");
+const video = document.querySelector('.flex');
+const speedBar = document.querySelector('.speed-bar');
+let isMouseDown = false;
 
-// Add event listeners
-video.addEventListener("click", togglePlay);
-video.addEventListener("play", updateButton);
-video.addEventListener("pause", updateButton);
-video.addEventListener("timeupdate", updateProgress);
-playButton.addEventListener("click", togglePlay);
-volumeSlider.addEventListener("input", handleVolumeChange);
-speedSlider.addEventListener("input", handleSpeedChange);
-skipButtons.forEach((button) => button.addEventListener("click", skip));
-speedBar.addEventListener("mousemove", handleSpeedBarMove);
-
-// Function to toggle play/pause
-function togglePlay() {
-  const method = video.paused ? "play" : "pause";
-  video[method]();
+function handleSpeedScrub(e) {
+  if (!isMouseDown) return;
+  const scrubY = e.pageY - this.offsetTop;
+  const percent = scrubY / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const playbackRate = percent * (max - min) + min;
+  speedBar.textContent = `${playbackRate.toFixed(1)}x`;
+  video.playbackRate = playbackRate;
 }
 
-// Function to update the play/pause button
-function updateButton() {
-  const icon = this.paused ? "►" : "❚ ❚";
-  playButton.textContent = icon;
-}
-
-// Function to update the progress bar
-function updateProgress() {
-  const percent = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasis = `${percent}%`;
-}
-
-// Function to handle volume change
-function handleVolumeChange() {
-  video.volume = this.value;
-}
-
-// Function to handle speed change
-function handleSpeedChange() {
-  video.playbackRate = this.value;
-}
-
-// Function to skip ahead or back
-function skip() {
-  video.currentTime += parseFloat(this.dataset.skip);
-}
-
-// Function to handle mouse movement on speed bar
-function handleSpeedBarMove(event) {
-  const barWidth = speedBar.offsetWidth;
-  const mouseX = event.offsetX;
-  const percent = (mouseX / barWidth) * 100;
-  const minSpeed = 0.5;
-  const maxSpeed = 4;
-  const speed = (percent / 100) * (maxSpeed - minSpeed) + minSpeed;
-  speedSlider.value = speed.toFixed(1);
-  video.playbackRate = speed;
-}
+speedBar.addEventListener('mousedown', () => isMouseDown = true);
+speedBar.addEventListener('mouseup', () => isMouseDown = false);
+speedBar.addEventListener('mouseleave', () => isMouseDown = false);
+speedBar.addEventListener('mousemove', handleSpeedScrub);
